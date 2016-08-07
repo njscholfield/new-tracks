b6f73a1d79cdafd29bce08ce37b32ac4b4e8c1b4
Noah Scholfield <contact@noahscholfield.com>
2016-08-06 22:43:25 -0400

    Change port for Heroku deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var tracks = require('./tracks.js');
 var app = express();
 
+app.set('port', process.env.PORT || 8000);
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
@@ -22,6 +23,6 @@ app.post('/:username/remove', function(req, res) {
   tracks.removeTrackFromList(req, res);
 });
 
-app.listen(8000, function() {
-  console.log('Node app listening on port 8000');
+app.listen(app.get('port'), function() {
+  console.log('Node app listening on port ' + app.get('port'));
 });
