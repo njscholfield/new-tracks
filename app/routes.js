283c249ab27e395f250fdd6b45517c0b99b1e238
Noah Scholfield <contact@noahscholfield.com>
2016-09-25 21:03:49 -0400

    Add register, login, soundcloud routes

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,28 @@
-module.exports = function(app, tracks) {
+module.exports = function(app, passport, tracks) {
+
+  app.get('/register', function(req, res) {
+    res.render('register', {username: "req.user", data: {}, error: {}, message: {type: 'text-danger', content: req.flash('signupMessage')}});
+  });
+
+  app.post('/register', function(req, res) {
+    console.log(req.body);
+    res.redirect('/');
+  });
+
+  app.get('/login', function(req, res) {
+    res.render('login', {message: req.flash('loginMessage'), error: {}});
+  });
+
+  app.post('/login', function(req, res) {
+    console.log(req.body);
+    res.redirect('/');
+  });
+
+  app.get('/auth/soundcloud', passport.authenticate('soundcloud'));
+
+  app.get('/auth/soundcloud/callback', passport.authenticate('soundcloud', { failureRedirect: '/login'}), function(req, res) {
+    res.redirect('/');
+  });
 
   app.get('/:username', function(req, res) {
     tracks.getTracks(req, res);
