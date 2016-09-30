254894b69c3ca3a1ae3ae172ead10b47fd185c73
Noah Scholfield <contact@noahscholfield.com>
2016-09-30 03:06:05 -0400

    Fix authentication, import models

--- tracks.js
+++ tracks.js
@@ -2,18 +2,23 @@
   need to check all and rewrite to reference username instead of permalink
   user will be stored in req.user so searching may not be necessary anymore
   createNewUser will definitely need to be changed
+  Rewrite database calls to be more efficient
 */
 
+var models = require('./app/userModel.js');
+var user = models.user;
+var track = models.track;
+
 exports.getTracks = function(req, res) {
-  if(req.params.username === req.get('username')) {
-    getTracksFromDatabase(req, res, req.params.username);
+  if(req.isAuthenticated() && req.user.username === req.params.username) {
+    getTracksFromDatabase(req, res, req.user.username);
   } else {
     res.sendStatus(403);
   }
 };
 
 var getTracksFromDatabase = function (req, res, username) {
-  user.findOne({permalink: username}, function(err, result) {
+  user.findOne({username: username}, function(err, result) {
     if(err) {
       console.log('Error searching database: ' + err);
       res.status(500).json({error: err});
@@ -25,21 +30,21 @@ var getTracksFromDatabase = function (req, res, username) {
 
 exports.editTrackInList = function(req, res) {
   var data = req.body;
-  if(req.params.username === data.permalink) {
-    editTrackInDatabase(req, res, data);
+  if(req.isAuthenticated() && req.user.username === req.params.username) {
+    editTrackInDatabase(req, res, data, req.user.username);
   } else {
     res.sendStatus(403);
   }
 };
 
-var editTrackInDatabase = function(req, res, data) {
+var editTrackInDatabase = function(req, res, data, username) {
   var newTrack = new track({
     title: data.title,
     artist: data.artist,
     releaseDate: data.releaseDate,
     trackID: data.trackID
   });
-  user.findOneAndUpdate({permalink: data.permalink, 'tracks.trackID': data.trackID}, {$set: {'tracks.$': newTrack}}, {new: true}, function(err, result) {
+  user.findOneAndUpdate({username: username, 'tracks.trackID': data.trackID}, {$set: {'tracks.$': newTrack}}, {new: true}, function(err, result) {
     if(err) {
       console.log('Error updating track: ' + err);
       res.status(400).json({error: err});
@@ -51,15 +56,15 @@ var editTrackInDatabase = function(req, res, data) {
 
 exports.removeTrackFromList = function(req, res) {
   var data = req.body;
-  if(req.params.username === data.permalink) {
-    removeTrackFromDatabase(req, res, data);
+  if(req.isAuthenticated() && req.user.username === req.params.username) {
+    removeTrackFromDatabase(req, res, data, req.user.username);
   } else {
     res.sendStatus(403);
   }
 };
 
-var removeTrackFromDatabase = function (req, res, data) {
-  user.findOneAndUpdate({permalink: data.permalink}, {$pull: {tracks: {trackID: data.trackID} } }, {new: true}, function(err, result) {
+var removeTrackFromDatabase = function (req, res, data, username) {
+  user.findOneAndUpdate({username: username}, {$pull: {tracks: {trackID: data.trackID} } }, {new: true}, function(err, result) {
     if(err) {
       console.log('Error removing track: ' + err);
       res.status(400).json({error: err});
@@ -71,15 +76,15 @@ var removeTrackFromDatabase = function (req, res, data) {
 
 exports.addTrackToList = function(req, res) {
   var data = req.body;
-  if(req.params.username === data.me.permalink) {
-    addTrackToDatabase(req, res, data, req.params.username);
+  if(req.isAuthenticated() && req.user.username === req.params.username) {
+    addTrackToDatabase(req, res, data, req.user.username);
   } else {
     res.sendStatus(403);
   }
 };
 
 var addTrackToDatabase = function(req, res, data, username) {
-  user.findOne({permalink: username}, function(err, result) {
+  user.findOne({username: username}, function(err, result) {
     if(err) {
       console.log('Error searching database: ' + err);
       res.status(400).json({error: err});
@@ -90,11 +95,7 @@ var addTrackToDatabase = function(req, res, data, username) {
         releaseDate: data.releaseDate,
         trackID: data.trackID
       });
-      if(result) {
-        addTrackToExistingUser(req, res, result, newTrack);
-      } else {
-        createNewUser(req, res, data, newTrack);
-      }
+      addTrackToExistingUser(req, res, result, newTrack);
     }
   });
 };
@@ -106,24 +107,6 @@ var addTrackToExistingUser = function(req, res, result, newTrack) {
       res.status(400).json({error: err});
     } else {
       res.status(200).json(result.tracks);
-    }
-  });
-};
-
-var createNewUser = function(req, res, data, newTrack) {
-  var newUser = new user({
-    fullName: data.me.full_name,
-    username: data.me.username,
-    permalink: data.me.permalink,
-    userID: data.me.user_id,
-    tracks: [newTrack]
-  });
-  newUser.save(function(err) {
-    if(err) {
-      console.log('Error saving newUser document: ' + err);
-      res.status(400).json({error: err});
-    } else {
-      res.sendStatus(200);
     }
   });
 };
