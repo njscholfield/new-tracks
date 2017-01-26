bd00905fb49c196ecefb5de4975a73370be56035
Noah Scholfield <contact@noahscholfield.com>
2017-01-26 15:26:09 -0500

    Add date posted to SoundCloud

--- static/js/soundcloud.js
+++ static/js/soundcloud.js
@@ -68,6 +68,7 @@
         sc.trackJSON = response.data;
         sc.html = JSONtoHTML(sc.trackJSON.description);
         sc.tags = processTags(sc.trackJSON.tag_list);
+        sc.postDate = new Date(sc.trackJSON.created_at);
         $scope.currentTrack = sc.trackJSON.id;
         if(sc.trackJSON.artwork_url) {
           sc.imgURL = sc.trackJSON.artwork_url.replace('large', 't500x500');
