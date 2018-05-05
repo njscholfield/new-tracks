16105517c5ebeb4c273aef0acb59b73d08484cc6
Noah Scholfield <contact@noahscholfield.com>
2018-05-05 17:08:21 -0400

    Create App.vue

---
+++ vue/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Navbar username="njscholfield"/>
+    <Input @update="updateData" />
+    <b-container>
+      <Description :raw-data="rawData" />
+    </b-container>
+  </div>
+</template>
+
+<script>
+  import Navbar from './Navbar.vue';
+  import Input from './Input.vue';
+  import Description from './Description.vue';
+  
+  export default {
+    data() {
+      return { 
+        rawData: {}
+      };
+    },
+    components: { Navbar, Input, Description },
+    methods: {
+      updateData(newData) {
+        this.rawData = newData;
+      }
+    }
+  }
+</script>
