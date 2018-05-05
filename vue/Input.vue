5b0ce084ca55b8cba2912985ba71b06b21c8245f
Noah Scholfield <contact@noahscholfield.com>
2018-05-05 17:08:55 -0400

    Create Input.vue

---
+++ vue/Input.vue
@@ -0,0 +1,79 @@
+<template>
+  <b-jumbotron>
+    <h3>Enter a track link below to get started</h3>
+    <form id="track-input" @submit.prevent="getTrackInfo">
+      <fieldset>
+        <div class="form-group">
+          <label class="sr-only" for="trackURL">SoundCloud® Track URL</label>
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text">
+                <font-awesome-icon :icon="soundcloud"/>
+              </span>
+            </div>
+            <input class="form-control has-feedback" type="url" v-model="trackURL" placeholder="https://soundcloud.com/...">
+            <div class="input-group-append">
+              <button class="form-control-feedback" aria-hidden="true" ng-click="descriptionCtrl.url = ''" type="reset"><font-awesome-icon :icon="times"/></button>
+            </div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  </b-jumbotron>
+</template>
+
+<script>
+  import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+  import { faSoundcloud } from '@fortawesome/fontawesome-free-brands';
+  import { faTimes } from '@fortawesome/fontawesome-free-solid';
+
+  export default {
+    data() {
+      return {
+        trackURL: '',
+        rawData: {},
+        soundcloud: faSoundcloud,
+        times: faTimes
+      };
+    },
+    components: { FontAwesomeIcon },
+    methods: {
+      getTrackInfo() {
+        const url = `https://api.soundcloud.com/resolve.json?url=${this.trackURL}&client_id=30cba84d4693746b0a2fbc0649b2e42c`;
+        fetch(url)
+          .then(blob => blob.json())
+          .then(data => {
+            this.rawData = data;
+            this.$emit('update', data);
+          })
+          .catch(response => console.log(response));
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .jumbotron {
+    background: #FEAC5E;
+    background: linear-gradient(to right, #4BC0C8, #C779D0, #FEAC5E); 
+    color: white;
+  }
+  @media (min-width: 576px) {
+    .jumbotron {
+      padding: 4rem;
+    }
+  }
+  .fa-soundcloud {
+    color: #f50;
+  }
+  input {
+    opacity: 0.6;
+  }
+  .form-control-feedback {
+    background-color: transparent;
+    color: white;
+    border: none;
+    margin-left: -1.5rem;
+    z-index: 10;
+  }
+</style>
