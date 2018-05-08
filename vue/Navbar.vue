7e908a7812f00a4604c7ad9627d9240ebcee171d
Noah Scholfield <contact@noahscholfield.com>
2018-05-08 00:38:38 -0400

    Fix image issue

--- vue/Navbar.vue
+++ vue/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <b-navbar toggleable="xl" type="dark" variant="primary">
-    <b-navbar-brand href="/"><img id="brandimg" :src="brandimg"></b-navbar-brand>
+    <b-navbar-brand href="/"><img id="brandimg" src="../static/favicon-96x96.png"></b-navbar-brand>
       <b-nav-item-dropdown right class="ml-auto" v-if="user.loggedIn">
         <!-- Using button-content slot -->
         <template slot="button-content">
@@ -19,13 +19,9 @@
 </template>
 
 <script>
-  import favicon from '../static/img/placeholder.png';
-  
   export default {
     data() {
-      return { 
-        brandimg: favicon
-      }
+      return {}
     },
     props: ['user'],
     computed: {
