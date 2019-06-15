b8a57cb0a1c47a870a1617cbc97e4fffc9ce6b15
Noah Scholfield <contact@noahscholfield.com>
2019-06-15 15:55:52 -0400

    Stop using parcel tree shaking because it doesn't work

--- vue/index.js
+++ vue/index.js
@@ -6,21 +6,27 @@ import VueAxios from 'vue-axios';
 
 Vue.use(VueAxios, axios);
 
-import Navbar from 'bootstrap-vue/es/components/navbar';
-import Alert from 'bootstrap-vue/es/components/alert';
-import Modal from 'bootstrap-vue/es/components/modal';
+import { NavbarPlugin, AlertPlugin, ModalPlugin } from 'bootstrap-vue';
 
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faCog, faTimes, faPencilAlt, faCheck, faStar, faSearch, faChevronUp, faMoon } from '@fortawesome/free-solid-svg-icons';
-import { faStar as farStar } from '@fortawesome/free-regular-svg-icons';
-import { faSoundcloud } from '@fortawesome/free-brands-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
+import { faCog } from '@fortawesome/free-solid-svg-icons/faCog';
+import { faTimes } from '@fortawesome/free-solid-svg-icons/faTimes';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons/faPencilAlt';
+import { faCheck } from '@fortawesome/free-solid-svg-icons/faCheck';
+import { faStar } from '@fortawesome/free-solid-svg-icons/faStar';
+import { faSearch } from '@fortawesome/free-solid-svg-icons/faSearch';
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons/faChevronUp';
+import { faMoon } from '@fortawesome/free-solid-svg-icons/faMoon';
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons/faStar';
+import { faSoundcloud } from '@fortawesome/free-brands-svg-icons/faSoundcloud';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 import App from './App.vue';
 
-Vue.use(Navbar);
-Vue.use(Alert);
-Vue.use(Modal);
+Vue.use(NavbarPlugin);
+Vue.use(AlertPlugin);
+Vue.use(ModalPlugin);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 library.add(faUser, faCog, faTimes, faPencilAlt, faCheck, faStar, faSearch, faChevronUp, faMoon, farStar, faSoundcloud);
 
